12aaf9372dd5de9877116636de50f1d89d6597fc
Matthew Walker <matthew.r.walker4@gmail.com>
2021-06-17 15:26:13 -0500

    completed mysql tables exercise

---
+++ albums_migration.sql
@@ -0,0 +1,13 @@
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE IF NOT EXISTS albums (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist VARCHAR(100) NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    release_date INT UNSIGNED NOT NULL,
+    sales DOUBLE UNSIGNED NOT NULL,
+    genre VARCHAR(150) NOT NULL,
+    PRIMARY KEY (id)
+);

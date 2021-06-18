0bc64fffc6d68bac7d9a360ef46095b7ed00c07c
Matthew Walker <matthew.r.walker4@gmail.com>
2021-06-18 12:32:31 -0500

    saved progress on mysql basic statements exercises

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,23 +1,17 @@
 USE codeup_test_db;
 
 
-SELECT 'The name of all albums by Pink Floyd.' AS 'Info';
-SELECT name FROM albums WHERE artist = 'Pink Floyd'
+SELECT name AS 'The name of all albums by Pink Floyd' FROM albums WHERE artist = 'Pink Floyd';
 
-SELECT 'The year Sgt. Pepper''s Lonely Hearts Club Band was released.' AS 'Info';
-SELECT release_date FROM albums WHERE name = 'Sgt.Pepper''s Lonely Hearts Club Band';
+SELECT release_date AS 'The year Sgt. Pepper''s Lonely Hearts Club Band was released.' FROM albums WHERE name = 'Sgt.Pepper''s Lonely Hearts Club Band';
 
-SELECT 'The genre for Nevermind.' AS 'Info';
-SELECT genre FROM albums WHERE name = 'Nevermind';
+SELECT genre AS 'The genre for Nevermind.' FROM albums WHERE name = 'Nevermind';
 
-SELECT 'Which albums were released in the 1990s' AS 'Info';
-SELECT * FROM albums WHERE release_date = 1990;
+SELECT name AS 'Which albums were released in the 1990s' FROM albums WHERE release_date BETWEEN 1990 AND 1999;
 
-SELECT 'Which albums had less than 20 million certified sales' AS 'Info';
-SELECT * FROM albums WHERE sales < 20;
+SELECT name AS 'Which albums had less than 20 million certified sales' FROM albums WHERE sales < 20;
 
 -- All the albums with a genre of "Rock".
 -- Why do these query results not include albums with a genre of "Hard rock" or "Progressive rock"?
 -- The query only shows results for the exact match of "Rock"
-SELECT 'All the albums with a genre of "Rock".' AS 'Info';
-SELECT * FROM albums WHERE genre = 'Rock';
+SELECT name AS 'All the albums with a genre of "Rock".' FROM albums WHERE genre = 'Rock';

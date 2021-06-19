5df99eb18089bedaba6702fca6b7ceeca506f890
Matthew Walker <matthew.r.walker4@gmail.com>
2021-06-19 12:47:35 -0500

    refactored some queries on select exercises

--- select_exercises.sql
+++ select_exercises.sql
@@ -7,11 +7,21 @@ SELECT release_date AS 'The year Sgt. Pepper''s Lonely Hearts Club Band was rele
 
 SELECT genre AS 'The genre for Nevermind.' FROM albums WHERE name = 'Nevermind';
 
-SELECT name AS 'Which albums were released in the 1990s' FROM albums WHERE release_date BETWEEN 1990 AND 1999;
+-- SELECT name AS 'Which albums were released in the 1990s' FROM albums WHERE release_date BETWEEN 1990 AND 1999;
 
-SELECT name AS 'Which albums had less than 20 million certified sales' FROM albums WHERE sales < 20;
+SELECT 'Which albums were released in the 1990s' AS 'Search results';
+SELECT * FROM albums WHERE release_date BETWEEN 1990 AND 1999;
+
+-- SELECT name AS 'Which albums had less than 20 million certified sales' FROM albums WHERE sales < 20;
+
+SELECT 'Which albums had less than 20 million certified sales' AS 'Search results';
+SELECT * FROM albums WHERE sales < 20;
+
 
 -- All the albums with a genre of "Rock".
 -- Why do these query results not include albums with a genre of "Hard rock" or "Progressive rock"?
--- The query only shows results for the exact match of "Rock"
-SELECT name AS 'All the albums with a genre of "Rock".' FROM albums WHERE genre = 'Rock';
+-- The query only shows results for the exact match of "Rock" or "rock".
+
+-- SELECT name AS 'All the albums with a genre of "Rock".' FROM albums WHERE genre = 'Rock';
+SELECT 'All the albums with a genre of "Rock".' AS 'Search results';
+SELECT * FROM albums WHERE genre = 'Rock';

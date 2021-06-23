393551426e040fd6f4bbe31c67e3d8c6b686cd64
Matthew Walker <matthew.r.walker4@gmail.com>
2021-06-23 13:22:50 -0500

    updated joins lec

--- joinsLEC.sql
+++ joinsLEC.sql
@@ -19,6 +19,7 @@ ON users.role_id = roles.id;
 
 USE codeup_test_db;
 
+# album_id = favorite album
 CREATE TABLE IF NOT EXISTS persons (
    person_id INT NOT NULL AUTO_INCREMENT,
    first_name VARCHAR(25) NOT NULL,
@@ -28,4 +29,41 @@ CREATE TABLE IF NOT EXISTS persons (
 
 INSERT INTO persons (first_name, album_id) VALUES ('Olivia', 29), ('Santiago', 27), ('Tareq', 15), ('Anaya', 28);
 
+# JOIN aka INNER JOIN
+# will only give you matches
+
 SELECT p.first_name, a.name FROM persons p JOIN albums a ON p.album_id = a.id;
+
+# left and right join are also called "outer joins"
+# will give you all rows from the selected table even without matches
+
+SELECT p.first_name, a.name FROM albums a LEFT JOIN persons p on a.id = p.album_id;
+
+SELECT p.first_name, a.name FROM persons p RIGHT JOIN albums a on a.id = p.album_id;
+
+# junction tables / associative tables / join tables
+
+CREATE TABLE preferences (
+     person_id INT NOT NULL,
+     album_id INT NOT NULL
+);
+
+INSERT INTO preferences (person_id, album_id)
+VALUES (1, 12), (1, 5), (1, 22), (1, 29), (2, 1), (2, 31), (2, 30), (3, 11), (3, 26), (3, 25);
+
+SELECT p.first_name AS name, a.name AS album FROM persons p JOIN preferences pf ON p.person_id = pf.person_id
+    JOIN albums a ON pf.album_id = a.id;
+
+# You can even circle back to a join, give it a different alias, and join to it again
+
+USE join_test_db;
+
+# 7 results predicted
+SELECT u.name AS name, ur.name AS role FROM users u JOIN roles ur ON u.role_id = ur.id;
+# 10 results predicted
+SELECT u.name AS name, ur.name AS role FROM users u LEFT JOIN roles ur ON u.role_id = ur.id;
+# ???
+SELECT u.name AS name, ur.name AS role FROM users u RIGHT JOIN roles ur ON u.role_id = ur.id;
+
+
+SELECT COUNT(*) AS users_in_each, ur.name AS role FROM users  JOIN roles ur ON users.role_id = ur.id GROUP BY role;

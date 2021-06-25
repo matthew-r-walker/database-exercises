e4b6f23e4e5429b77122c23f4e3f3e0f8e2e2a97
Matthew Walker <matthew.r.walker4@gmail.com>
2021-06-25 16:25:10 -0500

    completed join exercises

--- join_exercises.sql
+++ join_exercises.sql
@@ -1,30 +1,37 @@
 USE employees;
 
-SELECT CONCAT(e.first_name, ' ', e.last_name) AS 'Department Manager'
+SELECT d.dept_name AS 'Department Name', CONCAT(e.first_name, ' ', e.last_name) AS 'Department Manager'
 FROM employees e
-JOIN dept_emp de
-ON e.emp_no = de.emp_no
+JOIN dept_manager dm
+ON e.emp_no = dm.emp_no
 JOIN departments d
-ON de.dept_no = d.dept_no
-WHERE de.to_date LIKE '9%';
+ON dm.dept_no = d.dept_no
+WHERE dm.to_date LIKE '9%'
+ORDER BY dept_name;
 
 SELECT d.dept_name AS 'Department Name', CONCAT(e.first_name, ' ', e.last_name) AS 'Department Manager'
-FROM employees e JOIN dept_manager dm
-ON e.emp_no = dm.emp_no JOIN departments d LIMIT 10;
-
-# A solution
-
-SELECT d.dept_name AS 'Department Name',
-CONCAT(e.first_name, ' ', e.last_name) AS current_department_manager
-FROM employees AS e
-JOIN dept_manager AS dm ON e.emp_no = dm.emp_no
-AND to_date > CURDATE()
-JOIN departments AS d USING(dept_no)
+FROM employees e
+JOIN dept_manager dm
+ON e.emp_no = dm.emp_no
+JOIN departments d
+ON dm.dept_no = d.dept_no
+WHERE dm.to_date LIKE '9%' AND e.gender = 'F'
 ORDER BY dept_name;
 
 SELECT t.title, COUNT(t.title) AS Total
 FROM titles as t
-         JOIN dept_emp as de
-              ON de.emp_no = t.emp_no
+JOIN dept_emp as de
+ON de.emp_no = t.emp_no
 WHERE de.dept_no = 'd009' AND YEAR(t.to_date) = '9999' AND YEAR(de.to_date) = '9999'
 GROUP BY t.title;
+
+SELECT d.dept_name AS 'Department Name', CONCAT(e.first_name, ' ', e.last_name) AS 'Department Manager',
+s.salary AS Salary
+FROM employees e
+JOIN dept_manager dm
+ON e.emp_no = dm.emp_no
+JOIN departments d
+ON dm.dept_no = d.dept_no
+JOIN salaries s on e.emp_no = s.emp_no
+WHERE dm.to_date LIKE '9%' AND s.to_date LIKE '9%'
+ORDER BY dept_name;

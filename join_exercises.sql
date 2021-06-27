6944aac1eea04a1e982d8030eaf9c0d0d8313281
Matthew Walker <matthew.r.walker4@gmail.com>
2021-06-26 22:54:46 -0500

    added comments

--- join_exercises.sql
+++ join_exercises.sql
@@ -1,5 +1,7 @@
 USE employees;
 
+# Using the example in the Associative Table Joins section as a guide, write a query that shows each
+# department along with the name of the current manager for that department.
 SELECT d.dept_name AS 'Department Name', CONCAT(e.first_name, ' ', e.last_name) AS 'Department Manager'
 FROM employees e
 JOIN dept_manager dm
@@ -9,6 +11,7 @@ ON dm.dept_no = d.dept_no
 WHERE dm.to_date LIKE '9%'
 ORDER BY dept_name;
 
+# Find the name of all departments currently managed by women.
 SELECT d.dept_name AS 'Department Name', CONCAT(e.first_name, ' ', e.last_name) AS 'Department Manager'
 FROM employees e
 JOIN dept_manager dm
@@ -18,6 +21,7 @@ ON dm.dept_no = d.dept_no
 WHERE dm.to_date LIKE '9%' AND e.gender = 'F'
 ORDER BY dept_name;
 
+# Find the current titles of employees currently working in the Customer Service department.
 SELECT t.title, COUNT(t.title) AS Total
 FROM titles as t
 JOIN dept_emp as de
@@ -25,6 +29,7 @@ ON de.emp_no = t.emp_no
 WHERE de.dept_no = 'd009' AND YEAR(t.to_date) = '9999' AND YEAR(de.to_date) = '9999'
 GROUP BY t.title;
 
+# Find the current salary of all current managers.
 SELECT d.dept_name AS 'Department Name', CONCAT(e.first_name, ' ', e.last_name) AS 'Department Manager',
 s.salary AS Salary
 FROM employees e
